4b080880a6e899d8e7f25282f86ffbf3009a1bb8
Liam Slunjski <slunjski.liam@gmail.com>
2023-05-19 12:12:29 +0930

    Added first console output

--- FirstGitProject/FirstGitProject/FirstGitProject.cpp
+++ FirstGitProject/FirstGitProject/FirstGitProject.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "This is my first version controlled git project :)" << std::endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
